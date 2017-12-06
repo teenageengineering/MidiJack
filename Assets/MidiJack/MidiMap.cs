f15c534089556ed4f2043047f26d3ff3dcbfd72d
finnTE <finn@teenage.engineering>
2017-12-06 15:28:51 +0100

    Midi cc re-mapping for design & run time

---
+++ Assets/MidiJack/MidiMap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MidiJack
+{
+    [CreateAssetMenu(fileName = "MidiMap", menuName = "MidiJack/MidiMap")]
+    public class MidiMap : ScriptableObject
+    {
+        [Serializable]
+        public class Entry
+        {
+            public int from;
+            public int to;
+        }
+
+        public List<Entry> entries = new List<Entry>();
+
+        public int Map(int from)
+        {
+            Entry entry = entries.Find(e => e.from == from);
+            if (entry != null)
+                return entry.to;
+
+            return from;
+        }
+    }
+}
